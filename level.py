57ed09b2f389181ed2c0a34fe2f1ca3796b8ba9a
Jon Smith <aardvarkjon94@yahoo.com>
2021-12-05 22:00:42 -0600

    Used variable screen width for camera follow

--- level.py
+++ level.py
@@ -1,6 +1,6 @@
 import pygame
 from tiles import Tile
-from settings import tile_size
+from settings import tile_size, screen_height, screen_width
 from player import Player
 class Level:
     def __init__(self, level_data, surface):
@@ -27,10 +27,10 @@ class Level:
         player_x = player.rect.centerx
         direction_x = player.direction.x
 
-        if player_x < 200 and direction_x < 0:
+        if player_x < screen_width / 4 and direction_x < 0:
             self.world_shift = 8
             player.speed = 0
-        elif player_x > 1000 and direction_x > 0:
+        elif player_x > screen_width - screen_width / 4 and direction_x > 0:
             self.world_shift = -8
             player.speed = 0
         else:
