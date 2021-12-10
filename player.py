0fd9b9884fac08fc07ea7588dd4979e74c7165eb
Jon Smith <aardvarkjon94@yahoo.com>
2021-12-09 18:15:32 -0600

    Setup import function for animations

--- player.py
+++ player.py
@@ -12,6 +12,14 @@ class Player(pygame.sprite.Sprite):
         self.gravity = 0.8
         self.jump_speed = -16
         
+    def import_character_assets(self):
+        character_path = '../graphics/character/'
+        self.animation = {'idle': [],'run':[], 'jump':[], 'fall':[]}
+
+        for animation in self.animations.keys():
+            full_path = character_path + animation
+            self.animations[animation] = import_folder(full_path)
+
 
     def get_input(self):
         keys = pygame.key.get_pressed()
